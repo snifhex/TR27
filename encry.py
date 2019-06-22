8b477f65c699bfd9429dfee5ba5671589701f156
Sachin Tripathi <sachintripathis84@gmail.com>
2019-06-22 23:26:36 +0530

    Update encry.py

--- encry.py
+++ encry.py
@@ -1,1 +1,15 @@
 #for python3 
+
+
+def encrypt(txt, key):
+    alphabets = "abcdefghijklmnopqrstuvwxyz"    
+    punctuation = '! .-'
+    alphabets = alphabets*key
+    txt = txt.lower()
+    alpha = [ord(alpha) for alpha in list(alphabets)]
+    txt = ''.join([chr(alpha[alpha.index(ord(a))+key]) if a not in punctuation else a 
+        for a in list(txt)])
+    print(txt)
+    
+
+encrypt('hello This is sachin',2)
